b0d5251a1b4a2a1e2f5454dfc5dfa39dad2a6e3e
pannaf <panna@cr8tr.ai>
2024-06-14 11:30:22 -0700

    adding password protection

--- app2.py
+++ app2.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import hmac
 from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
 from src.prompts.yaml_prompt_loader import YamlPromptLoader
@@ -73,6 +74,36 @@ st.set_page_config(
         """,
     },
 )
+
+st.info("Join the waitlist!")
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input("Password", type="password", on_change=password_entered, key="password")
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
 st.title("V : AI Personal Trainer :muscle: :woman-lifting-weights: :superhero:")
 
 # Initialize the assistant system
